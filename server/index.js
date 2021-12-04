c5e5eb046cc89f4c078147e7eafc5b8c3dbc0464
freschwamm <fredreschwamm@outlook.com>
2021-12-04 20:49:04 +0000

    set up route function to call jetti sales api using get request

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const express = require('express')
-
+const router = require('./router/')
+const bodyParser = require("body-parser");
 const app = express()
 const port = 3000
 
 
-//defining routes - $app ->get
-
-app.get('/', (request, response) => {
-    response.send('Hello World')
-})
+app.use(bodyParser.json());
+app.use(express.json())
 
 
-// starting the server on port 3000
+router(app)
+
+
 app.listen(port)
