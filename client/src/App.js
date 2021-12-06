d09605bc4f804dda100f9f4192f942f88a36f677
freschwamm <fredreschwamm@outlook.com>
2021-12-06 14:31:33 +0000

    added react frontend

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SalesData from "./components/SalesData/SalesData";
+
+function App() {
+  return (
+    <div className="App">
+  <SalesData/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/SalesData/SalesData.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from "react";
+
+
+const SalesData = () => {
+
+
+    // const [allSales, setAllSales] = useState([]);
+    //
+    //
+    //
+    // const jettiSales = async () => {
+    //     const response = await fetch("http://localhost:3000")
+    //     let jettiSalesData = await response.json();
+    //     setAllSales(jettiSalesData)
+    //
+    // }
+    //
+    //
+    // console.log(allSales)
+
+
+    return (
+        <div>
+        </div>
+    );
+}
+export default SalesData
